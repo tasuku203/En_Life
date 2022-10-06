2d8aaa0f53240055ecdc1c10e0af9b5173020ab5
hideyama0101@gmail.com <ec2-user@ip-172-31-20-174.ap-northeast-1.compute.internal>
2022-10-06 23:54:26 +0000

    [Add]admin,userコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
