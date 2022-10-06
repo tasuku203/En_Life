2904c56471383ad2495bbabeb9613994455a8e0f
hideyama0101@gmail.com <ec2-user@ip-172-31-20-174.ap-northeast-1.compute.internal>
2022-10-06 22:07:08 +0000

    [Add]homesコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,16 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :user, skip: [:password], controllers: {
     registrations: "user/registrations",
     sessions: "user/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+ scope module: :users do
+   get "/", to: "homes#top"
+ end
+
+
 end
