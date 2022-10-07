fce429870d972af741587eba81a09cc1662a71f8
hideyama0101@gmail.com <ec2-user@ip-172-31-20-174.ap-northeast-1.compute.internal>
2022-10-07 23:54:54 +0000

    [Update]deviseデフォルトのルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-
-  devise_for :admin, controllers: {
+# password変更とadmin側のregistration機能は今回不要なルーティングなのでskipオプションで取り消す
+# 生成したコントローラーadmin,usersがどこに存在しているのかを記述
+  devise_for :admin,skip: [:password, :registrations,], controllers: {
     sessions: "admin/sessions"
   }
-  devise_for :users
+  devise_for :users, skip: [:password,], controllers: {
+    sessinos: "users/sessions",
+    registrations: "users/registration"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
