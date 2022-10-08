06f60ff59c038d06e3cdbc10178ef7aa0ad91907
hideyama0101@gmail.com <ec2-user@ip-172-31-20-174.ap-northeast-1.compute.internal>
2022-10-08 00:34:41 +0000

    [Add]adminのsignin,userのsignup,signin画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 # password変更とadmin側のregistration機能は今回不要なルーティングなのでskipオプションで取り消す
 # 生成したコントローラーadmin,usersがどこに存在しているのかを記述
-  devise_for :admin,skip: [:password, :registrations,], controllers: {
-    sessions: "admin/sessions"
-  }
-  devise_for :users, skip: [:password,], controllers: {
+
+
+  devise_for :users, controllers: {
     sessinos: "users/sessions",
-    registrations: "users/registration"
+    registrations: "users/registrations"
+  }
+
+  devise_for :admin,skip: [:password, :registrations], controllers: {
+    sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
