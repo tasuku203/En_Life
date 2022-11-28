0359034196b94ad21266d6662d1fbf0178815e43
hideyama0101@gmail.com <ec2-user@ip-172-31-20-174.ap-northeast-1.compute.internal>
2022-11-28 08:53:49 +0000

    [Change]rubyのバージョンを下げた

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -1,10 +1,9 @@
 class Users::HomesController < ApplicationController
   def top
-    # @user = User.find(current_user.id)
-    # @followings = @user.followings
-    # @posts = Post.where(user_id: @followings).order(created_at: :DESC).page(params[:page]).per(4)
-
-    # @posts_all = Post.all.order(created_at: :DESC).page(params[:page]).per(4)
+    @user = User.find(current_user.id)
+    @followings = @user.followings
+    @posts = Post.where(user_id: @followings).order(created_at: :DESC).page(params[:page]).per(4)
+    @posts_all = Post.all.order(created_at: :DESC).page(params[:page]).per(4)
   end
 
   def about
